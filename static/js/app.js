d65de911fb479bf4616153be277b4d87328a4d3c
cbebry <invalidsyntax@gmail.com>
2016-05-29 23:53:33 -0400

    Table setup

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,14 @@
-angular.module('app', ['app.modules'
+angular.module('app', ['ngTable', 'app.modules'
         ])
 	//.config(['$urlRouterProvider',
 	//         function($urlRouterProvider) {
 	//	//Set default route
 	//	$urlRouterProvider.otherwise('/');
 	//}])
+	.run(function(ngTableDefaults) {
+		ngTableDefaults.params.count = 5;
+    	ngTableDefaults.settings.counts = [];
+	})
 ;
 
 angular.module('app.modules', [
--- static/js/workout/workout.controller.js
+++ static/js/workout/workout.controller.js
@@ -1,11 +1,26 @@
 (function() {
 	'use strict';
 
-	function WorkoutCtrl() {
+	function WorkoutCtrl(NgTableParams) {
 		var vm = this;
-		vm.test = 'pass';
+
+
+		vm.data = [
+			{ exercise: 'dumbbell curl', weight: 25, sets: 3, reps: 8, comments: 'left arm weaker' },
+			{ exercise: 'bench press', weight: 120, sets: 3, reps: 8, comments: 'terminated on last rep' },
+			{ exercise: 'overhead extension', weight: 50, sets: 3, reps: 8, comments: 'favorite exercise' },
+			{ exercise: 'leg curl', weight: 140, sets: 3, reps: 8, comments: '' },
+			{ exercise: 'barbell row', weight: 80, sets: 3, reps: 8, comments: '' },
+		];
+
+		vm.tableParams = new NgTableParams({
+			// initial sort order
+			sorting: { name: "asc" }
+		}, {
+			data: vm.data 
+		});
 	};
 
 	angular.module('workout')
-		.controller('WorkoutCtrl', [WorkoutCtrl]);
+		.controller('WorkoutCtrl', ['NgTableParams', WorkoutCtrl]);
 })();
