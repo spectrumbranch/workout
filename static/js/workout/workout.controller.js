6879ea8a34ee3d6fab47fb40bcb1cf2dc019fe1b
cbebry <invalidsyntax@gmail.com>
2016-05-30 02:23:18 -0400

    Added dummy action icons

--- static/js/workout/workout.controller.js
+++ static/js/workout/workout.controller.js
@@ -6,11 +6,11 @@
 
 
 		vm.data = [
-			{ exercise: 'dumbbell curl', weight: 25, sets: 3, reps: 8, comments: 'left arm weaker' },
-			{ exercise: 'bench press', weight: 120, sets: 3, reps: 8, comments: 'terminated on last rep' },
-			{ exercise: 'overhead extension', weight: 50, sets: 3, reps: 8, comments: 'favorite exercise' },
-			{ exercise: 'leg curl', weight: 140, sets: 3, reps: 8, comments: '' },
-			{ exercise: 'barbell row', weight: 80, sets: 3, reps: 8, comments: '' },
+			{ id: 1, exercise: 'dumbbell curl', weight: 25, sets: 3, reps: 8, comments: 'left arm weaker' },
+			{ id: 2, exercise: 'bench press', weight: 120, sets: 3, reps: 8, comments: 'terminated on last rep' },
+			{ id: 3, exercise: 'overhead extension', weight: 50, sets: 3, reps: 8, comments: 'favorite exercise' },
+			{ id: 4, exercise: 'leg curl', weight: 140, sets: 3, reps: 8, comments: '' },
+			{ id: 5, exercise: 'barbell row', weight: 80, sets: 3, reps: 8, comments: '' },
 		];
 
 		vm.tableParams = new NgTableParams({
@@ -19,6 +19,13 @@
 		}, {
 			data: vm.data 
 		});
+
+		vm.editExercise = function(id) {
+			console.log('TODO edit exercise: ', id);
+		}
+		vm.deleteExercise = function(id) {
+			console.log('TODO delete exercise: ', id);
+		}
 	};
 
 	angular.module('workout')
